43411bcb9f0b25b4085b76b8bc22ecaf317f3faf
faedhuil <aljoschaschneider@web.de>
2025-11-25 08:50:04 +0100

    formatiert

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,30 +1,28 @@
 package com.cc.java;
 
 public class App {
-    
+
     public static void main(String[] args) {
         Konto konto1 = new Konto();
         Konto konto2 = new Konto();
         Konto konto3 = new Konto();
 
-        output("Konto1: " + konto1.getKontostand());
-        output("Konto2: " + konto2.getKontostand());
-        output("Konto3: " + konto3.getKontostand());
+        output("Konto1: " + konto1.getKontostand() + "€");
+        output("Konto2: " + konto2.getKontostand() + "€");
+        output("Konto3: " + konto3.getKontostand() + "€");
 
-        konto1.setKontostand(konto1.getKontostand()*2);
-        konto2.setKontostand(konto2.getKontostand()*3);
-        konto3.setKontostand(konto3.getKontostand()*10);
-        
+        konto1.setKontostand(konto1.getKontostand() * 2);
+        konto2.setKontostand(konto2.getKontostand() * 3);
+        konto3.setKontostand(konto3.getKontostand() * 10);
 
         output("--------------------------------");
-        output("Konto1: " + konto1.getKontostand());
-        output("Konto2: " + konto2.getKontostand());
-        output("Konto3: " + konto3.getKontostand());
+        output("Konto1: " + konto1.getKontostand() + "€");
+        output("Konto2: " + konto2.getKontostand() + "€");
+        output("Konto3: " + konto3.getKontostand() + "€");
     }
-
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
-    } 
+    }
 
 }
