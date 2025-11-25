0b2cd3847352c4aeeea4cff0ead7bfb669db94b3
Aljoscha Schneider <Aljoschaschneider@web.de>
2025-11-25 01:57:03 +0100

    finished

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,23 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-    
+        Konto konto1 = new Konto();
+        Konto konto2 = new Konto();
+        Konto konto3 = new Konto();
+
+        output("Konto1: " + konto1.getKontostand());
+        output("Konto2: " + konto2.getKontostand());
+        output("Konto3: " + konto3.getKontostand());
+
+        konto1.setKontostand(konto1.getKontostand()*2);
+        konto2.setKontostand(konto2.getKontostand()*3);
+        konto3.setKontostand(konto3.getKontostand()*10);
+        
+
+        output("--------------------------------");
+        output("Konto1: " + konto1.getKontostand());
+        output("Konto2: " + konto2.getKontostand());
+        output("Konto3: " + konto3.getKontostand());
     }
 
 
---
+++ com/cc/java/Konto.java
@@ -0,0 +1,20 @@
+package com.cc.java;
+
+public class Konto {
+
+
+    private int kontostand = 10000;
+
+    public Konto() {
+        
+    }
+
+    public int getKontostand() {
+        return kontostand;
+    }
+
+    public void setKontostand(int kontostand) {
+        this.kontostand = kontostand;
+    }
+
+}
